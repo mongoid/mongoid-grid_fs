mongoid/mongoid-grid_fs

commit a5aee5d2207ea9de1bebe3192554b7057422cefa

Fixed error#10128 in doing File#each when total size exceeds 42mb.
[Fixes #16]

lib/mongoid-grid_fs.rb
@@ -204,6 +204,7 @@
             attributes[:length] ||= length
             attributes[:uploadDate] ||= Time.now.utc
             attributes[:md5] ||= md5.hexdigest
+            attributes[:chunkLength] = chunks.size
 
             file.update_attributes(attributes)
 
@@ -342,8 +343,15 @@
           end
 
           def each(&block)
-            chunks.all.order_by([:n, :asc]).each do |chunk|
-              block.call(chunk.to_s)
+            fetched, limit = 0, 7
+
+            while fetched < chunkLength
+              chunks.where(:n.lt => fetched+limit, :n.gte => fetched).
+                order_by([:n, :asc]).each do |chunk|
+                  block.call(chunk.to_s)
+                end
+
+              fetched += limit
             end
           end
 

commit ed560e10013471fa0ff36e2eaea1503d64a9be03

Fixed a typo introduced in a5aee5d220 of ahoward/mongoid-grid_fs, chunkLength should be chunks.size.

lib/mongoid-grid_fs.rb
@@ -345,7 +345,7 @@
           def each(&block)
             fetched, limit = 0, 7
 
-            while fetched < chunkLength
+            while fetched < chunks.size
               chunks.where(:n.lt => fetched+limit, :n.gte => fetched).
                 order_by([:n, :asc]).each do |chunk|
                   block.call(chunk.to_s)